682116bd4673cb9928fcef27da110af00a65468e
Luka Korosec <luka.korosec13@yahoo.com>
2025-04-21 08:33:09 +0200

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -30,18 +30,18 @@
     mojObjekt.prototype = {
 
       //let je beseda ki definera mojo škatlico za informacije, temu se reče varjabla
-      let observer = new MutationObserver(txtHandler); //tle ukradem objekt ki mi ga da na razbolagko API/bibilioteka v naprej sprogramiranih miljon objektov ki mi jih nudijo
+      let observer = new MutationObserver(txtHandler); //tle ukradem objekt ki mi ga da na razbolagko API/bibilioteka v naprej sprogramiranih miljon objektov ki mi jih nudijo iz APIjov.
       let elTarget = document.getElementById("gesture-text"); // tle na spletni strani sam že uporabim že predefinirano funkcijo k mi jo nudi JavaScript, in <div>/element is spletne u mojo škatlco.
-      let objConfig = { childList: !0, subtree: !0, attributes: !1, characterData: !1 }; // tle definerate parametre moja poslušalca, k sem ga sami ustvarl. Pr predefineranmu poslušalcu dogotkov tega ne rabte.
+      let objConfig = { childList: !0, subtree: !0, attributes: !1, characterData: !1 }; // tle definerate parametre moja poslušalca, k sem ga sami ustvarl. Pri predefineranmu poslušalcu dogotkov tega ne rabte.
   
-      //observe je predefineran v js, pošljušalec dogotkov. Teh posljšalcev imate predefiniranih malo morje. Kot je click, ki posluša, če kdo klikne z miško na neki.
+      //observe je predefineran v js, pošljušalec dogotkov. Teh posljšalcev imate predefiniranih malo morje. Kot je click, ki posluša, če kdo klikne z miško na neki itd -> google.
       observer.observe(elTarget, objConfig);
           //spet ena varjabla, nič druzga k škatlica za informacijo
-          let lastMove = 0;
+          let lastMove = 0; // to rabim za ustavit poslušalca, dostkrat pri umetni inteligenci
           //moja funkcija, nič druzga kt da rečem kaj se nej zgodi
           function txtHandler() {
               //ker dobim preveč dogotkov, moram poslušalec ustavt za 0.8 sekund(pogosto pri umetni inteligenci), da mi ne pobezla vse tko. Pri "click" tega ne rabite, 
-              // ker je samo en event, pač samo uporabnik enkrat klikne na zadevo. Umetna inteligenca ti ponavad vrže 1000če dogotkov
+              // ker je samo en event, pač samo uporabnik enkrat klikne na zadevo. Umetna inteligenca ti ponavad vrže 1000če dogotkov v sekundi. (je druga verzija rešitve sam pustmo), mi gledamo JavaScirpt
               if (Date.now() - lastMove > 800) {
                   //tuki napišem kaj se nej zgodi, k bo moj poslušalec dogotkov (event listener), poslal informacijo, če se bo kej zgodil
                   
@@ -50,7 +50,8 @@
                   //(kup stvari mi ni treba definirat, k so jih oni, in je skoz upToDate). -> https://mootools.net/core/docs/1.6.0 (vsi primeri predefinirah funkcij, kako se velik krajš vse piše)
                   let e = $("gesture-text").outerText.toString()/*tle definiram informacijo k jo dobim iz elementa spletne strani <div>*/, t = $("socialD"), i = $("socialL");
 
-                  //drugič tle povem kaj mi nej nardi u primeru če dobim informacijo k jo želim, se pravi na podlagi informacije sporži ali funcijo "socialD", ali funkcijo "socialL" na dogodek/event klik miške.
+                  //drugič tle povem kaj mi nej nardi u primeru če dobim informacijo k jo želim, se pravi na podlagi informacije sporži ali funcijo "socialD", ali funkcijo "socialL" 
+                  // na dogodek/event klik miške. tega sem napiremer definiral v totalno drugem objektu, in odpru varjable celem programu -> global variabla -> googli
                   "right" === e /*e sem definiru vrstico višji, je moja varjabla*/ ? t.fireEvent("click", "socialD") : "left" === e && i.fireEvent("click", "socialL"), 
                 
 
